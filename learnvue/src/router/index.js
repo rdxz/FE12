08493bc222b2b0063f212245b07784a7c7f0a47f
shudong <wang@126.com>
2017-12-19 10:16:18 +0800

    add route

--- learnvue/src/router/index.js
+++ learnvue/src/router/index.js
@@ -5,6 +5,16 @@ import Article from '@/components/Article'
 
 const Study = {template:`<div>this study page</div>`}
 const Work = {template:`<div>this work page</div>`}
+const About = { template: `<div> 我是About组件 <router-view> </router-view> </div>` };
+const Blog = { template:`<div>this Blog page</div>`}
+const Info = { template:`<div>this Info page</div>`}
+// const User = {
+//   template: '<div>User</div>'
+// }
+import User from '@/components/User'
+// const User = {
+//   template: '<div>User {{ $route.params.id }}</div>'
+// }
 
 Vue.config.productionTip = false
 Vue.use(Router)
@@ -13,7 +23,10 @@ export default new Router({
   linkActiveClass:'shudong',
   mode:'history',
   // mode:'hash',
-  routes:[{
+  routes:[
+    // 动态路径参数 以冒号开头
+  { path: '/user/:id?', component: User },
+  {
     path:'/',
     name:'hello',
     component:Stark
@@ -30,5 +43,19 @@ export default new Router({
     path: '/work',
     name: 'work',
     component: Work 
-  }]
+  },
+    {path: '/about',  // 这是一级路由
+    component: About,
+    children: [{  // 里面是嵌套路由
+      path: 'blog',  //如果在这个嵌套
+      name: 'blog',
+      component: Blog
+    },
+    {
+      path: '/info',
+      name: 'info',
+      component: Info
+    }
+    ]}
+  ]
 })
